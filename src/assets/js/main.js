62f27beccac9e3cd4c6669fe5f21cc047bb409e0
jumaelmartins <jumaelmartins@gmail.com>
2023-03-04 21:48:34 -0300

    Responsividade

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -134,3 +134,10 @@ document.addEventListener("click", (e)=> {
   loadLocalStorageItem(shortenLinkToRender);
   // deleteItemLocalStorage(shortenLinkToRender);
 })
+
+/*
+<svg width="64" height="64" class="trash" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <path d="M18 6 6 18"></path>
+  <path d="m6 6 12 12"></path>
+</svg> 
+*/
